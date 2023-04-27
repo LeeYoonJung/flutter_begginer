650b9e7bb49d6d0a3b4f420d2c2697a44c8a1924
LeeYoonJung <imj1017@naver.com>
2023-04-27 17:20:06 +0900

    main List 구현 및 AnimatedOpacity 기능 구현

---
+++ lib/beginner/0_flutter_cookbook/my_animated_opacity.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedOpacity extends StatefulWidget {
+  const MyAnimatedOpacity({Key? key}) : super(key: key);
+
+  @override
+  State<MyAnimatedOpacity> createState() => _MyAnimatedOpacityState();
+}
+
+class _MyAnimatedOpacityState extends State<MyAnimatedOpacity> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AnimatedOpacity'),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 500),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.pinkAccent,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+    ;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_momong_project/beginner/0_flutter_cookbook/my_animated_opacity.dart';
 
 import 'beginner/0_flutter_cookbook/my_animated_container.dart';
 
@@ -17,54 +18,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyAnimatedContainer(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Tutorial'),
+        ),
+        body: const Home(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return ListView(
+      children: <Widget>[
+        const ListTile(
+          title: Text(
+            '섹션0.Flutter Cookbook',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ListTile(
+          title: const Text('AnimatedContainer'),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyAnimatedContainer()));
+          },
+        ),
+        ListTile(
+          title: const Text('AnimatedOpacity'),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyAnimatedOpacity()));
+          },
+        ),
+      ],
     );
   }
 }
