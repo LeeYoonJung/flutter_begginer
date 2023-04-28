ce597653663dae0b7a1f179d27cf29bbf07dd58d
LeeYoonJung <imj1017@naver.com>
2023-04-28 17:11:19 +0900

    TextField(decoration, focus 기능), AlertDialog 기능

--- lib/beginner/0_flutter_cookbook/my_form_validation.dart
+++ lib/beginner/0_flutter_cookbook/my_form_validation.dart
@@ -8,7 +8,25 @@ class MyFormValidation extends StatefulWidget {
 }
 
 class _MyFormValidationState extends State<MyFormValidation> {
- final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+  late FocusNode nameFocusNode;
+
+  final nameController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nameFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    nameFocusNode.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,33 +34,70 @@ class _MyFormValidationState extends State<MyFormValidation> {
       appBar: AppBar(
         title: const Text('Form Validation'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return '공백은 허용되지 않습니다.';
-              }
-            },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // form 안에 있을 경우 validate 기능 사용할 경우
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '공백은 허용되지 않습니다.';
+                  }
+                },
+              ),
+              //RaisedButton() deprecated
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // valid
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(content: Text('처리중')));
+                    }
+                  },
+                  child: const Text('완료'),
+                ),
+              ),
+              // form 안에 있을 경우 validate 기능 사용 안할 경우
+              TextField(
+                controller: nameController,
+                onChanged: (text) {
+                  print(text);
+                },
+                focusNode: nameFocusNode,
+                decoration: const InputDecoration(
+                  hintText: '이름을 입력해주세요.',
+                  border: InputBorder.none,
+                  labelText: '이름',
+                ),
+                autofocus: true,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(nameFocusNode);
+                },
+                child: const Text('포커스'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(nameController.text);
+                  showDialog(context: context, builder: (context){
+                    return AlertDialog(
+                      content: Text(nameController.text),
+                    );
+                  });
+                },
+                child: const Text('TextFeild 값 확인'),
+              ),
+            ],
           ),
-          //RaisedButton() deprecated
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if(_formKey.currentState!.validate()) {
-                  // valid
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('처리중')));
-                }
-              },
-              child: const Text('완료'),
-            ),
-          )
-        ],
-      ),),
+        ),
+      ),
     );
   }
 }
